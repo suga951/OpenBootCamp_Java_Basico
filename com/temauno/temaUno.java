c4ae96889d949ad0902e52953af72c6fed8b3554
Agustin Lopez <aalopezba@gmail.com>
2023-03-03 13:07:12 -0300

    first commit

---
+++ com/temauno/temaUno.java
@@ -0,0 +1,27 @@
+package com.temauno;
+
+public class temaUno {
+
+    public static void main(String[] args) {
+
+        byte datoByte = 2;
+        short datoShort = 4;
+        float datoFloat = 6.6f;
+        double datoDouble = 12.12d;
+        int datoInt = 24;
+        long datoLong = 48;
+
+        String datoString = "Esto es un tipo de dato!";
+        char datoChar = 'a';
+
+        System.out.println("Esto es un dato primitivo tipo byte: " + datoByte);
+        System.out.println("Esto es un dato primitivo tipo short: " + datoShort);
+        System.out.println("Esto es un dato primitivo tipo float: " + datoFloat);
+        System.out.println("Esto es un dato primitivo tipo double: " + datoDouble);
+        System.out.println("Esto es un dato primitivo tipo int: " + datoInt);
+        System.out.println("Esto es un dato primitivo tipo long: " + datoLong);
+        System.out.println("Esto es un dato primitivo tipo String: " + datoString);
+        System.out.println("Esto es un dato primitivo tipo char: " + datoChar);
+
+    }
+}
