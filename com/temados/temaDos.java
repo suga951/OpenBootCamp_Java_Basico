5bca08687584c05a92621e697541b1896eb968b0
Agustin Lopez <aalopezba@gmail.com>
2023-03-03 14:02:10 -0300

    ejercicio dos

--- com/temados/temaDos.java
+++ com/temados/temaDos.java
@@ -1,2 +1,20 @@
-package com.temados;public class temaDos {
+package com.temados;
+
+public class temaDos {
+
+    public static void main(String[] args) {
+
+        double precio = 199.95d;
+
+
+        System.out.println("El precio con IVA es de: " + valorTotal(precio));
+        System.out.println("El IVA es de: " + iva(precio));
+    }
+    static double iva(double valor){
+        return (valor/100) * 21;
+    }
+    static double valorTotal(double precio){
+        double total = iva(precio) + precio;
+        return total;
+    }
 }
