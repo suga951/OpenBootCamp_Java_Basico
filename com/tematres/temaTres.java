82524717cbefe0553b2a342ab09cb4c41f10b870
Agustin Lopez <aalopezba@gmail.com>
2023-03-04 16:22:22 -0300

    tema tres

---
+++ com/tematres/temaTres.java
@@ -0,0 +1,18 @@
+package com.tematres;
+
+
+public class temaTres {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Mishima","Murakami","Ishiguro","Murata","Soseki","Arikawa"};
+
+        for(int i = 0; i < nombres.length; i++){
+
+            System.out.print(nombres[i]+ " ");
+
+        }
+
+    }
+
+}
+
